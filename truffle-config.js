848923d14b9389905366e947610cc2033958041c
Panov Dmitriy <panov92@gmail.com>
2021-11-05 02:23:44 +0300

    set up ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
-require('babel-register')
-require('babel-polyfill')
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
@@ -7,6 +12,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*', // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   contracts_directory: './src/contracts/',
